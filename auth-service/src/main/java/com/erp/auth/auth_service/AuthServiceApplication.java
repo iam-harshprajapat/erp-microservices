dc6f7ed831e2759390238132eb6ad850b57cac89
Priyanka Patel <pn.priyankapatel@gmail.com>
2026-01-14 00:17:36 +0530

    Update AuthServiceApplication.java

--- auth-service/src/main/java/com/erp/auth/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/erp/auth/auth_service/AuthServiceApplication.java
@@ -13,7 +13,7 @@ public class AuthServiceApplication {
     @PostConstruct
     public void debugDb() {
         System.out.println("DB_URL=" + System.getenv("DB_URL"));
-        System.out.println("DB_USER=" + System.getenv("DB_USER"));
+        System.out.println("DB_USER=" + System.getenv("DB_USERNAME"));
         System.out.println("DB_PASSWORD=" + (System.getenv("DB_PASSWORD") != null));
     }
 }
